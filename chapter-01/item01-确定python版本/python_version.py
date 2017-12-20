033f5d1eced2e05366844e96d4244fea3fd6f129
jieyan <852353298@qq.com>
2017-12-21 00:19:25 +0800

    effective-python-59-specific-ways

---
+++ chapter-01/item01-确定python版本/python_version.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/21 上午12:05
+# @Author  : Aries
+# @Site    : 
+# @File    : python_version.py
+# @Software: PyCharm
+'''
+确定python版本
+'''
+import sys
+
+if __name__ == '__main__':
+    print(sys.version_info)
+    print(sys.version)
+    # sys.version_info(major=3, minor=5, micro=0, releaselevel='final', serial=0)
+    # 3.5.0 (v3.5.0:374f501f4567, Sep 12 2015, 11:00:19)
+    # [GCC 4.2.1 (Apple Inc. build 5666) (dot 3)]
+
+'''
+1. 开发项目优先考虑python 3,因为python 2在2018年停止更新
+2. 项目中的python版本应该和系统版本一致
+'''
+#
---
+++ chapter-01/item02-PEP8代码风格/pep8-style.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/21 上午12:12
+# @Author  : Aries
+# @Site    : 
+# @File    : pep8-style.py
+# @Software: PyCharm
+'''
+PEP8 代码风格
+'''
