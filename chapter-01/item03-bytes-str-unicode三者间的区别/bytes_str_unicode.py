26fae10c83e28d11319dd9eb0f9d20c403b4a61e
tazdingobean <jieyan@jiedeMacBook-Pro.local>
2017-12-26 10:31:40 +0800

    PEP代码风格

---
+++ chapter-01/item03-bytes-str-unicode三者间的区别/bytes_str_unicode.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/25 下午10:49
+# @Author  : Aries
+# @Site    : 
+# @File    : bytes_str_unicode.py
+# @Software: PyCharm
+'''
+
+'''
